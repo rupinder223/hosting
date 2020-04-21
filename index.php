4fa0aab0a9791b3111b01c7847e0c4470c2c3534
rupinder223 <64055606+rupinder223@users.noreply.github.com>
2020-04-20 23:29:08 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyFirstHosting</title>
+    <style>
+   body {
+  background-color: #C1BBBA;
+}
+
+h1 {
+  color: black;
+  text-align: center;
+}
+
+p {
+  font-family: cursive;
+  font-size: 20px;
+}
+</style>
+</head>
+<body>
+<h1> Rupinder's First Hosting </h1>
+    <h2> Welcome here</h2>
+    <p> This is just for hosting using PHP language. This is my first hosting.</p>
+</body>
+</html>
